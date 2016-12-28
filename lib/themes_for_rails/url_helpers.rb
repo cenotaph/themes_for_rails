b189a899eb7456843d451e0f9d6e3b20ac0647e6
John W. Fail <john@pixelache.ac>
2016-12-28 12:15:29 +0200

    Update url_helpers.rb

--- lib/themes_for_rails/url_helpers.rb
+++ lib/themes_for_rails/url_helpers.rb
@@ -1,27 +1,27 @@
 # encoding: utf-8
 module ThemesForRails
   module UrlHelpers
-
     extend ActiveSupport::Concern
-    
     included do
+      include ThemesForRails::CommonMethods
       helper_method :current_theme_stylesheet_path, 
         :current_theme_javascript_path, 
         :current_theme_image_path
     end
-    
-    def current_theme_stylesheet_path(asset)
-      base_theme_stylesheet_path(:theme => self.theme_name, :asset => "#{asset}.css")
+    module InstanceMethods
+      def current_theme_stylesheet_path(asset)
+        #base_theme_stylesheet_path(:theme => self.theme_name, :asset => "#{asset}.css")
+        "/themes/#{self.theme_name}/stylesheets/#{asset}.css"
+      end
+      def current_theme_javascript_path(asset)
+        #base_theme_javascript_path(:theme => self.theme_name, :asset => "#{asset}.js")
+        "/themes/#{self.theme_name}/javascripts/#{asset}.js"
+      end
+      def current_theme_image_path(asset)
+        image, extension = asset.split(".")
+        #base_theme_image_path(:theme => self.theme_name, :asset => "#{image}.#{extension}")
+        "/themes/#{self.theme_name}/images/#{asset}"
+      end
     end
-
-    def current_theme_javascript_path(asset)
-      base_theme_javascript_path(:theme => self.theme_name, :asset => "#{asset}.js")
-    end
-
-    def current_theme_image_path(asset)
-      image, extension = asset.split(".")
-      base_theme_image_path(:theme => self.theme_name, :asset => "#{image}.#{extension}")
-    end
-
   end
 end
