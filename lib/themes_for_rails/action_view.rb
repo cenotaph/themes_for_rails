f6e6f1be1c86cfb4e9277728a5df0671da413cd5
John W. Fail <john@pixelache.ac>
2016-12-28 12:46:08 +0200

    going back

--- lib/themes_for_rails/action_view.rb
+++ lib/themes_for_rails/action_view.rb
@@ -10,18 +10,18 @@ module ThemesForRails
     end
 
     def current_theme_stylesheet_path(asset)
-      #base_theme_stylesheet_path(:theme => self.theme_name, :asset => "#{asset}.css")
-      "/themes/#{self.theme_name}/stylesheets/#{asset}.css"
+      base_theme_stylesheet_path(:theme => self.theme_name, :asset => "#{asset}.css")
+      #"/themes/#{self.theme_name}/stylesheets/#{asset}.css"
     end
     
     def current_theme_javascript_path(asset)
-      #base_theme_javascript_path(:theme => self.theme_name, :asset => "#{asset}.js")
-      "/themes/#{self.theme_name}/javascripts/#{asset}.js"
+      base_theme_javascript_path(:theme => self.theme_name, :asset => "#{asset}.js")
+      #"/themes/#{self.theme_name}/javascripts/#{asset}.js"
     end
 
     def current_theme_image_path(asset)
-      #base_theme_image_path(:theme => self.theme_name, :asset => asset)
-      "/themes/#{self.theme_name}/images/#{asset}"
+      base_theme_image_path(:theme => self.theme_name, :asset => asset)
+      #"/themes/#{self.theme_name}/images/#{asset}"
     end
 
     def theme_stylesheet_path(asset, new_theme_name = self.theme_name)
@@ -33,8 +33,8 @@ module ThemesForRails
     end
 
     def theme_image_path(asset, new_theme_name = self.theme_name)
-      #base_theme_image_path(:theme => new_theme_name, :asset => asset)
-      "/themes/#{new_theme_name}/images/#{asset}"
+      base_theme_image_path(:theme => new_theme_name, :asset => asset)
+      #"/themes/#{new_theme_name}/images/#{asset}"
     end
     
     def theme_image_tag(source, options = {})
