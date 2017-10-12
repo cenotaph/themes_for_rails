c47ecab827f86442a3f73af4c3fb427ca36aa1f2
John W. Fail <john@pixelache.ac>
2017-10-12 13:46:13 +0300

    removing alias_method_chain

--- lib/themes_for_rails/action_mailer.rb
+++ lib/themes_for_rails/action_mailer.rb
@@ -7,14 +7,14 @@ module ThemesForRails
 
     included do
       include ThemesForRails::ActionController
-      alias_method_chain :mail, :theme
+#       alias_method_chain :mail, :theme
     end
 
     def mail_with_theme(headers = {}, &block)
       theme_opts = headers[:theme] || self.class.default[:theme]
       theme(theme_opts) if theme_opts
 
-      mail_without_theme(headers, &block)
+      mail(headers, &block)
     end
     
   end
