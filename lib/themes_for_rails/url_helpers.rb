813d84fe8673b375ceb3b4d4b2acbb9c4b74c1df
John W. Fail <john@pixelache.ac>
2017-11-11 12:55:01 +0200

    Update url_helpers.rb

--- lib/themes_for_rails/url_helpers.rb
+++ lib/themes_for_rails/url_helpers.rb
@@ -4,9 +4,11 @@ module ThemesForRails
     extend ActiveSupport::Concern
     included do
       include ThemesForRails::CommonMethods
-      helper_method :current_theme_stylesheet_path, 
-        :current_theme_javascript_path, 
-        :current_theme_image_path
+       if respond_to?(:helper_method)
+         helper_method :current_theme_stylesheet_path, 
+          :current_theme_javascript_path, 
+          :current_theme_image_path
+       end
     end
     module InstanceMethods
       def current_theme_stylesheet_path(asset)
